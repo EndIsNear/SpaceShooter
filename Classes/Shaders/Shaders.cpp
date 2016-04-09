62e9eab8ea469a44ec8928624b913965db7c4979
NDrobrev <martindobrev0@gmail.com>
2016-04-09 23:04:52 +0300

    Improve shader with texture map for edge area

--- Classes/Shaders/Shaders.cpp
+++ Classes/Shaders/Shaders.cpp
@@ -10,18 +10,41 @@ GLProgram * GetOnHitShader()
 	p->bindAttribLocation(GLProgram::ATTRIBUTE_NAME_COLOR, GLProgram::VERTEX_ATTRIB_COLOR);
 	p->bindAttribLocation(GLProgram::ATTRIBUTE_NAME_TEX_COORD, GLProgram::VERTEX_ATTRIB_TEX_COORDS);
 	p->link();
+	p->use();
 	p->updateUniforms();
+	
+	auto textureCache = Director::getInstance()->getTextureCache();
+	auto tex1 = textureCache->addImage("fighterShieldMask.png");
+
+	
+
+	auto tex1Loc = p->getUniformLocationForName("CC_TextureMask");
+	p->setUniformLocationWith1i(tex1Loc, 1);
+
+	glBindTexture(GL_TEXTURE_2D, tex1->getName());
+	glActiveTexture(GL_TEXTURE1);
+
+	auto tex2 = textureCache->addImage("fighterShieldMask.png");
+	auto tex2Loc = p->getUniformLocationForName("CC_Texture0");
+	p->setUniformLocationWith1i(tex2Loc, 0);
+
+	glBindTexture(GL_TEXTURE_2D, tex2->getName());
+	glActiveTexture(GL_TEXTURE0);
+
 
 	return p;
 }
 
 void UpdateOnHitShader(GLProgram *p, const float time)
 {
+
 	GLProgramState* state = GLProgramState::getOrCreateWithGLProgram(p);
+
 	GLuint ctime = glGetUniformLocation(p->getProgram(), "u_ctime");
 	state->setUniformFloat(ctime, fmod(time, 1.f));
 	GLuint gtime = glGetUniformLocation(p->getProgram(), "u_gtime");
 	state->setUniformFloat(gtime, 1.f);
 	GLuint color = glGetUniformLocation(p->getProgram(), "u_color");
-	state->setUniformVec4(color, cocos2d::Vec4(0.5f, 0.f, 0.f, 1.f));
+	state->setUniformVec4(color, cocos2d::Vec4(0.f, 0.7f, 0.7f, 1.f));
 }
+
--- Resources/Shaders/OnHit.fsh
+++ Resources/Shaders/OnHit.fsh
@@ -4,6 +4,7 @@ varying vec2 v_texCoord;
 uniform float u_ctime;
 uniform float u_gtime;
 uniform vec4 u_color;
+uniform sampler2D CC_TextureMask;
 
 float easeOutCubic(float t, float b, float c, float d) {
 	t /= d;
@@ -12,14 +13,22 @@ float easeOutCubic(float t, float b, float c, float d) {
 }
 
 void main() {
-	vec4 tex = texture2D(CC_Texture0, v_texCoord);
+	
+	vec4 mask =  texture2D(CC_TextureMask, v_texCoord);
+	if(mask.x > 0.0001)
+	{
+		vec4 tex = texture2D(CC_Texture0, v_texCoord);
 
-	float progress = easeOutCubic(u_ctime, -2.0, 3.0, u_gtime);
-	progress += v_texCoord.y;
+		float progress = easeOutCubic(u_ctime, -2.0, 3.0, u_gtime);
+		progress += v_texCoord.y;
 
-	float diff = v_texCoord.x - progress;
-	if (diff <= 0.8 && diff > 0.0) {
-		tex = tex + (u_color * diff) * tex.a * u_color.a;
+		float diff = v_texCoord.x - progress;
+		if (diff <= 0.8 && diff > 0.0) {
+			tex = tex + (u_color * diff) * tex.a * u_color.a;
+		}
+		
+		gl_FragColor =v_fragmentColor * tex *mask.xxxx;
 	}
-	gl_FragColor = tex * v_fragmentColor;
+	else
+		gl_FragColor = texture2D(CC_Texture0, v_texCoord);
 }
