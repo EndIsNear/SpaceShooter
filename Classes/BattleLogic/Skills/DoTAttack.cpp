fce535e0debf8c521de1e6082fa1fac4a154e5e6
EndIsNear <alexanderdespotov@gmail.com>
2016-04-16 10:22:49 +0300

    Implement DoT skill

---
+++ Classes/BattleLogic/Skills/DoTAttack.cpp
@@ -0,0 +1,18 @@
+#include "BattleLogic\Skills\DoTAttack.h"
+#include "BattleLogic\LogicalShip.h"
+
+SkillResult DoTAttack::OnHit()
+{
+	SkillResult res;
+	res.m_Effect = SkillEffect(SkillEffect::EffectType::DoT, EffectFunc([this](LogicalShip* ship, const float dt, float timeEffect)
+	{
+		if (timeEffect > 0.f)
+		{
+			ship->TakeDmg(m_Dmg * dt);
+			timeEffect -= dt;
+			return true;
+		}
+		return false;
+	}));
+	return res;
+}
---
+++ Classes/BattleLogic/Skills/DoTAttack.h
@@ -0,0 +1,36 @@
+#ifndef  __DOT_ATACK_H__
+#define  __DOT_ATACK_H__
+
+#include "BattleLogic\Skills\SkillBase.h"
+
+class DoTAttack : public SkillInterface
+{
+	float m_Dmg;//this dmg is dmg per second
+	float m_BulletSpeeed;
+	std::string m_SpriteName;
+public:
+	DoTAttack(const float dmg, const float cdn, const float bulletSpeed, const std::string& spriteName)
+		: m_Dmg(dmg), m_BulletSpeeed(bulletSpeed), m_SpriteName(spriteName) 
+	{
+		m_MaxCooldown = cdn;
+	}
+	virtual SkillInterface * Clone() override { return new DoTAttack(m_Dmg, m_MaxCooldown, m_BulletSpeeed, m_SpriteName); };
+	virtual ~DoTAttack() override {}
+
+	/// Used when there is command to cast a skill
+	virtual SkillResult OnCast(const cocos2d::Vec2 pos, const cocos2d::Vec2 dir) override
+	{
+		SkillResult res;
+		res.m_Source = this;
+		res.m_Bullet = new BulletBase(pos, dir.getNormalized(), m_BulletSpeeed, m_SpriteName);
+		
+		m_CrnCooldown = m_MaxCooldown;
+		assert(res.m_Bullet != nullptr);
+		return res;
+	}
+
+	/// Used after the projectile hit the enemy
+	virtual SkillResult OnHit() override;
+};
+
+#endif // __DOT_ATACK_H__
